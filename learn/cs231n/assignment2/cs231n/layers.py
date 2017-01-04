418bf70c4beae0a5cd8d0e27090edf9ae02d1cc0
Leonardo Araujo <leonardo.araujo@apical.co.uk>
2017-01-04 17:47:32 +0000

    Solving the Fully-Connected general network, from assignemnt 2 of cs231n

--- learn/cs231n/assignment2/cs231n/layers.py
+++ learn/cs231n/assignment2/cs231n/layers.py
@@ -24,7 +24,14 @@ def affine_forward(x, w, b):
   # TODO: Implement the affine forward pass. Store the result in out. You     #
   # will need to reshape the input into rows.                                 #
   #############################################################################
-  pass
+  # Get number of items
+  N = x.shape[0]
+
+  # Reshape to have N rows and as many collumns as needed
+  x_temp = x.reshape(N,-1)
+
+  # Simple linear operation, after this step just cache the inputs
+  out = x_temp.dot(w) + b
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -52,7 +59,18 @@ def affine_backward(dout, cache):
   #############################################################################
   # TODO: Implement the affine backward pass.                                 #
   #############################################################################
-  pass
+  # Get number of items
+  N = x.shape[0]
+
+  # Reshape to have N rows and as many collumns as needed
+  x_temp = x.reshape(N,-1)
+
+  # Calculate the gradients, for reference check:
+  # https://leonardoaraujosantos.gitbooks.io/artificial-inteligence/content/fc_layer.html
+  # http://cs231n.github.io/optimization-2/#mat
+  db = np.sum(dout, axis = 0)
+  dw = x_temp.T.dot(dout)
+  dx = dout.dot(w.T).reshape(x.shape)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -74,7 +92,9 @@ def relu_forward(x):
   #############################################################################
   # TODO: Implement the ReLU forward pass.                                    #
   #############################################################################
-  pass
+  # Simple Relu forward propagation
+  relu_func = lambda x: x * (x > 0).astype(float)
+  out = relu_func(x)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -97,7 +117,8 @@ def relu_backward(dout, cache):
   #############################################################################
   # TODO: Implement the ReLU backward pass.                                   #
   #############################################################################
-  pass
+  # The backward pass is just dout on the cases where x were positive (kind of mask)
+  dx = dout * (x >= 0)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
