4c54c94e95e27c1b341bdd413d6f1a05e4af9713
laraujo <leonardoaraujo.santos@gmail.com>
2017-01-20 00:39:43 +0000

    Adding singleton pattern to LossFactory

--- loss/LossFactory.m
+++ loss/LossFactory.m
@@ -1,20 +1,25 @@
-classdef LossFactory < handle
+classdef (Sealed) LossFactory < handle
     %LOSSFACTORY Summary of this class goes here
-    %   Detailed explanation goes here
+    % References: https://uk.mathworks.com/help/matlab/matlab_oop/controlling-the-number-of-instances.html
     
     properties
     end
     
     methods (Static)
+        % Singleton pattern for loss function
         function lossInstance = GetLoss(lossType)
-            switch lossType
-                case 'cross_entropy'
-                    lossInstance = CrossEntropy();
-                case 'mean_squared_error'
-                    lossInstance = MeanSquaredError();
-                otherwise
-                    lossInstance =  [];
+            persistent localObj
+            if isempty(localObj) || ~isvalid(localObj)                
+                switch lossType
+                    case 'cross_entropy'
+                        localObj = CrossEntropy();
+                    case 'mean_squared_error'
+                        localObj = MeanSquaredError();
+                    otherwise
+                        localObj =  [];
+                end
             end
+            lossInstance = localObj;
         end
     end
     
