56684db83fe2dcd6699f28eeff0cec24b2a34163
leonardo <leonardoaraujo.santos@gmail.com>
2017-01-28 02:09:03 +0000

    Playing with MNIST 86%

--- test/test_Simple_2_layer_FC_MNIST.m
+++ test/test_Simple_2_layer_FC_MNIST.m
@@ -10,14 +10,22 @@ rng(0,'v5uniform');
 load mnist_oficial;
 data = Dataset(input_train, output_train_labels,1,784,1,1,true);
 data.AddValidation(input_test,output_test_labels,1,784,1,1,true);
-%data.pushToGPU();
+data.pushToGPU();
 
 %% Create network
 layers = LayerContainer();    
 layers <= struct('name','ImageIn','type','input','rows',1,'cols',784,'depth',1, 'batchsize',1);
-layers <= struct('name','FC_1','type','fc', 'num_output',50);
+layers <= struct('name','FC_1','type','fc', 'num_output',20);
+%layers <= struct('name','BN_1','type','batchnorm','eps',1e-5, 'momentum', 0.9);
 layers <= struct('name','Relu_1','type','relu');
-layers <= struct('name','FC_2','type','fc','num_output',data.GetNumClasses());
+layers <= struct('name','FC_2','type','fc', 'num_output',20);
+%layers <= struct('name','BN_2','type','batchnorm','eps',1e-5, 'momentum', 0.9);
+layers <= struct('name','Relu_2','type','relu');
+%layers <= struct('name','FC_3','type','fc', 'num_output',200);
+%layers <= struct('name','BN_3','type','batchnorm','eps',1e-5, 'momentum', 0.9);
+%layers <= struct('name','Relu_3','type','relu');
+%layers <= struct('name','DRP_1','type','dropout','prob',0.5);
+layers <= struct('name','FC_4','type','fc','num_output',data.GetNumClasses());
 layers <= struct('name','Softmax','type','softmax');
 
 % Create DeepLearningModel instance
@@ -25,15 +33,16 @@ net = DeepLearningModel(layers, LossFactory.GetLoss('multi_class_cross_entropy')
 
 
 %% Create solver and train
-solver = Solver(net, data, 'sgd',containers.Map({'learning_rate', 'L2_reg'}, {0.01, 0}));
+solver = Solver(net, data, 'sgd',containers.Map({'learning_rate', 'L2_reg'}, {0.001, 0}));
 solver.SetBatchSize(200);
 solver.SetEpochs(100);
 solver.Train();
 
 %% Test
+testBatchSize = size(input_test,1);
 figure(2);
-batchValidation = data.GetValidationBatch(10);
-display_MNIST_Data(reshape_row_major(batchValidation.X,[10,784]));
+batchValidation = data.GetValidationBatch(testBatchSize);
+%display_MNIST_Data(reshape_row_major(batchValidation.X,[testBatchSize,784]));
 title('Images on validation');
 errorCount = 0;
 
@@ -46,13 +55,13 @@ idxScoresMax = idxScoresMax - 1;
 idxCorrect = idxCorrect - 1;
 
 % Compare scores with target
-for idx=1:10    
+for idx=1:testBatchSize    
     if idxScoresMax(idx) ~= idxCorrect(idx)
         errorCount = errorCount + 1;
-        fprintf('Predicted %d and should be %d\n',idxScoresMax(idx),idxCorrect(idx));
+        %fprintf('Predicted %d and should be %d\n',idxScoresMax(idx),idxCorrect(idx));
     end    
 end
-errorPercentage = (errorCount*100) / 10;
+errorPercentage = (errorCount*100) / testBatchSize;
 fprintf('Accuracy is %d percent \n',round((100-errorPercentage)));
 figure;
 plot(solver.GetLossHistory)
