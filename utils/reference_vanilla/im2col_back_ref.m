ea3b7e6cd40bd75d48e015c1d1b9038f772cf804
leonardo <leonardoaraujo.santos@gmail.com>
2017-02-09 01:31:37 +0000

    Adding comments

--- utils/reference_vanilla/im2col_back_ref.m
+++ utils/reference_vanilla/im2col_back_ref.m
@@ -1,8 +1,8 @@
 function [ img_grad ] = im2col_back_ref( dout, dout_H, dout_W, S, HH, WW, C )
 %IM2COL_BACK_REF Backpropagation of im2col
-% dout: (
+% dout: Input of im2col backprop
 % Return
-% Image gradient (H,W,C)
+% Im2col gradient w.r.t to input times dout
 
 % Calculate the spatial dimensions of im_grad
 % Observe that the result will be "padded"
@@ -12,6 +12,7 @@ W = (dout_W - 1) * S + WW;
 img_grad = zeros(H,W,C);
 
 for ii=1:(dout_H*dout_W)
+    % Select row from dout
     row = dout(ii,:);
     
     % Create a patch from the row
@@ -21,9 +22,12 @@ for ii=1:(dout_H*dout_W)
     % Calculate indexes on dx
     h_start = floor(((ii-1) / dout_W) * S);    
     w_start = mod((ii-1),dout_W) * S;
+    
+    % We increment due to the fact that matlab arrays start at 1
     h_start = h_start + 1;
     w_start = w_start + 1;
         
+    % Increment img_grad with patch
     img_grad(h_start:h_start+HH-1, w_start:w_start+WW-1, :) = img_grad(h_start:h_start+HH-1, w_start:w_start+WW-1, :) + patch;    
 end
 
