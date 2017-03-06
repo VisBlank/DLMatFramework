9db395113de33b735abe3060ba7a842eaf0b5ea4
Leonardo Araujo <leonardo.araujo@apical.co.uk>
2017-03-06 17:32:39 +0000

    Starting work on issue #9

---
+++ utils/data_structure/GraphAdjMatrix.m
@@ -0,0 +1,21 @@
+classdef GraphAdjMatrix < Handle
+    %GRAPHADJMATRIX Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Access = 'protected')
+        m_adjacentMatrix = [];
+        m_headNode = [];
+    end
+    
+    methods (Access = 'public')
+        function AddEdges(obj, node1, node2)
+            
+        end
+        
+        function PrintGraph(obj)
+            
+        end
+    end
+    
+end
+
