7598cac581a2d4f94a6f4c38a6752970a1ea9e0a
leonardo <leonardoaraujo.santos@gmail.com>
2017-03-19 00:26:58 +0000

    Moving Tensor code to .cpp file, issue #45

--- utils/optimization_functions/cppPrj/utils/tensor.cpp
+++ utils/optimization_functions/cppPrj/utils/tensor.cpp
@@ -11,3 +11,147 @@ http://stackoverflow.com/questions/115703/storing-c-template-function-definition
 
 // Explicit declare available versions to avoid linker error.
 //template class Tensor<float>;
+
+template<typename T>
+Tensor<T>::Tensor(const vector<int> &dims, initializer_list<T> list){
+    m_buffer = vector<T>(list.size(),0);
+    copy(list.begin(), list.end(), m_buffer.begin());
+    SetDims(dims);
+}
+
+template<typename T>
+Tensor<T>::Tensor(initializer_list<T> list){
+    m_buffer = vector<T>(list.size(),0);
+    copy(list.begin(), list.end(), m_buffer.begin());
+}
+
+template<typename T>
+Tensor<T>::Tensor(const vector<int> &dims):m_dims(dims){
+    m_num_dims = dims.size();
+    SetDims(dims);
+    // Initialize vector with prodDims size and fill with zeros
+    m_buffer = vector<T>(m_numElements,0);
+}
+
+template<typename T>
+void Tensor<T>::SetDims(const vector<int> &dims){
+    m_dims = dims;
+    m_num_dims = dims.size();
+    // Do a "prod" of all elements on vector
+    int prodDims = 1;
+    for_each(dims.begin(), dims.end(), [&] (int m){prodDims *= m;});
+    m_numElements = prodDims;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator+(const T b) const {
+    // Create result tensor with same dimensions
+    Tensor<T> result(vector<int>({m_dims}));
+
+    // For each element of m_buffer multiply by b and store the result on resVec
+    transform(m_buffer.begin(), m_buffer.end(), result.begin(),std::bind1st(std::plus<T>(),b));
+
+    return result;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator*(const T b) const{
+    // Create result tensor with same dimensions
+    Tensor<T> result(vector<int>({m_dims}));
+
+    // For each element of m_buffer multiply by b and store the result on resVec
+    transform(m_buffer.begin(), m_buffer.end(), result.begin(),std::bind1st(std::multiplies<T>(),b));
+
+    return result;
+}
+
+template<typename T>
+void Tensor<T>::print() const{
+    auto start = m_buffer.begin();
+    auto ncols = m_dims[1];
+    auto nrows = m_dims[0];
+    for (int i = 0; i < nrows; ++i){
+        // Get a slice from the vector
+        vector<T> rowSlice(start, start + m_dims[1]);
+        cout << "| ";
+        for_each(rowSlice.begin(), rowSlice.end(), [](T m){cout << m << " ";});
+        start += ncols;
+        cout << "|" << endl;
+    }
+}
+
+template<typename T>
+T &Tensor<T>::operator()(int row, int col){
+    // Using at is safer because it checks the boundaries of the vector
+    //return m_buffer[MAT_2D(row, col,m_dims[1])];
+    return m_buffer.at(MAT_2D(row, col,m_dims[1]));
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator*(Tensor &b){
+    vector<int> bDims = b.GetDims();
+    if (b.GetNumDims() > 2){
+        throw invalid_argument("Only 2d matrix multiplication is supported.");
+    }
+    // Create result matrix
+    Tensor<T> result(vector<int>({m_dims[0],bDims[1]}));
+    int num_rows_A = m_dims[0];
+    int num_cols_A = m_dims[1];
+    int num_cols_B = bDims[1];
+    for(int i=0; i<num_rows_A; ++i) {
+#pragma omp parallel for
+        for (int k=0; k<num_cols_B; ++k) {
+            T sum = 0;
+            for (int j=0; j<num_cols_A; ++j){
+                sum += (*this)(i,j) * b(j,k);
+            }
+            result(i,k) = sum;
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator+(const Tensor &b) const {
+    // Create result tensor with same dimensions
+    Tensor<T> result(vector<int>({m_dims}));
+    vector<int> bDims = b.GetDims();
+    if (bDims != m_dims){
+        throw invalid_argument("Dimensions must match.");
+    }
+
+    // Add contents of A and B and store results on resVec
+    transform(m_buffer.begin(), m_buffer.end(), b.begin(),result.begin(), plus<T>());
+
+    return result;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator-(const Tensor &b) const{
+    // Create result tensor with same dimensions
+    Tensor<T> result(vector<int>({m_dims}));
+    vector<int> bDims = b.GetDims();
+    if (bDims != m_dims){
+        throw invalid_argument("Dimensions must match.");
+    }
+
+    // Add contents of A and B and store results on resVec
+    transform(m_buffer.begin(), m_buffer.end(), b.begin(),result.begin(), minus<T>());
+
+    return result;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator=(const Tensor &b){
+    // Create result tensor with same dimensions
+    Tensor<T> result(vector<int>({m_dims}));
+    copy(b.begin(), b.end(), result.begin());
+    return result;
+}
+
+/*
+ * Explicit declare template versions to avoid linker error. (This is needed if we use templates on .cpp files)
+*/
+template class Tensor<float>;
+template class Tensor<double>;
+template class Tensor<int>;
--- utils/optimization_functions/cppPrj/utils/tensor.h
+++ utils/optimization_functions/cppPrj/utils/tensor.h
@@ -42,37 +42,16 @@ private:
 public:
     // Delete default Constructor
     //Tensor() = delete;
-    Tensor(){};
-    Tensor(const vector<int> &dims, initializer_list<T> list){
-        m_buffer = vector<T>(list.size(),0);
-        copy(list.begin(), list.end(), m_buffer.begin());
-        SetDims(dims);
-    }
-    Tensor(initializer_list<T> list){
-        m_buffer = vector<T>(list.size(),0);
-        copy(list.begin(), list.end(), m_buffer.begin());
-    }
+    Tensor(){}
+    Tensor(const vector<int> &dims, initializer_list<T> list);
+    // Allows aggregate initialization
+    //http://en.cppreference.com/w/cpp/language/aggregate_initialization
+    Tensor(initializer_list<T> list);
 
-    void SetDims(const vector<int> &dims){
-        m_dims = dims;
-        m_num_dims = dims.size();
-        // Do a "prod" of all elements on vector
-        int prodDims = 1;
-        for_each(dims.begin(), dims.end(), [&] (int m){prodDims *= m;});        
-        m_numElements = prodDims;
-    }
+    Tensor (const vector<int> &dims);
 
-    Tensor (const vector<int> &dims):m_dims(dims){
-        m_num_dims = dims.size();
-        // Do a "prod" of all elements on vector
-        int prodDims = 1;
-        for_each(dims.begin(), dims.end(), [&] (int m){prodDims *= m;});
-        // Initialize vector with prodDims size and fill with zeros
-        m_buffer = vector<T>(prodDims,0);
-        m_numElements = prodDims;
-    }
+    void SetDims(const vector<int> &dims);
 
-    // A const method does not change it's class members
     // The expected format will be rows,cols,channel,batch
     int GetNumDims() const {return m_num_dims;}
     vector<int> GetDims() const {return m_dims;}
@@ -95,107 +74,19 @@ public:
         return m_buffer.end();
     }
 
-    // The cont here means that this method will not change the class members    
-    void print() const{
-        auto start = m_buffer.begin();
-        auto ncols = m_dims[1];
-        auto nrows = m_dims[0];
-        for (int i = 0; i < nrows; ++i){
-            // Get a slice from the vector
-            vector<T> rowSlice(start, start + m_dims[1]);
-            cout << "| ";
-            for_each(rowSlice.begin(), rowSlice.end(), [](T m){cout << m << " ";});
-            start += ncols;
-            cout << "|" << endl;
-        }
-    }
+    void print() const;
 
     /*
-    Overload the "()" and "*" operators to make it feel like matlab
-  */
-    T& operator()(int row, int col){
-        // Using at is safer because it checks the boundaries of the vector
-        //return m_buffer[MAT_2D(row, col,m_dims[1])];
-        return m_buffer.at(MAT_2D(row, col,m_dims[1]));
-    }
-
-    Tensor<T> operator*(Tensor &b){
-        vector<int> bDims = b.GetDims();
-        if (b.GetNumDims() > 2){
-            throw invalid_argument("Only 2d matrix multiplication is supported.");
-        }
-        // Create result matrix
-        Tensor<T> result(vector<int>({m_dims[0],bDims[1]}));
-        int num_rows_A = m_dims[0];
-        int num_cols_A = m_dims[1];
-        int num_cols_B = bDims[1];
-        for(int i=0; i<num_rows_A; ++i) {
-            #pragma omp parallel for
-            for (int k=0; k<num_cols_B; ++k) {
-                T sum = 0;
-                for (int j=0; j<num_cols_A; ++j){
-                    sum += (*this)(i,j) * b(j,k);
-                }
-                result(i,k) = sum;
-            }
-        }
-        return result;
-    }
-
-    Tensor<T> operator*(const T b) const{
-        // Create result tensor with same dimensions
-        Tensor<T> result(vector<int>({m_dims}));       
-
-        // For each element of m_buffer multiply by b and store the result on resVec
-        transform(m_buffer.begin(), m_buffer.end(), result.begin(),std::bind1st(std::multiplies<T>(),b));
-
-        return result;
-    }
-
-    Tensor<T> operator+(const Tensor &b) const {
-        // Create result tensor with same dimensions
-        Tensor<T> result(vector<int>({m_dims}));
-        vector<int> bDims = b.GetDims();
-        if (bDims != m_dims){
-            throw invalid_argument("Dimensions must match.");
-        }        
-
-        // Add contents of A and B and store results on resVec
-        transform(m_buffer.begin(), m_buffer.end(), b.begin(),result.begin(), plus<T>());
-
-        return result;
-    }
-
-    Tensor<T> operator+(const T b) const {
-        // Create result tensor with same dimensions
-        Tensor<T> result(vector<int>({m_dims}));
-
-        // For each element of m_buffer multiply by b and store the result on resVec
-        transform(m_buffer.begin(), m_buffer.end(), result.begin(),std::bind1st(std::plus<T>(),b));
-
-        return result;
-    }
-
-    Tensor<T> operator-(const Tensor &b) const {
-        // Create result tensor with same dimensions
-        Tensor<T> result(vector<int>({m_dims}));
-        vector<int> bDims = b.GetDims();
-        if (bDims != m_dims){
-            throw invalid_argument("Dimensions must match.");
-        }        
-
-        // Add contents of A and B and store results on resVec
-        transform(m_buffer.begin(), m_buffer.end(), b.begin(),result.begin(), minus<T>());
-
-        return result;
-    }
-
-    Tensor<T> operator=(const Tensor &b){
-        // Create result tensor with same dimensions
-        Tensor<T> result(vector<int>({m_dims}));        
-        copy(b.begin(), b.end(), result.begin());
-        return result;
-    }
+        Overload the "()" and "*" operators to make it feel like matlab
+        The const after the method definition means that this method will not change the class members
+    */
+    T& operator()(int row, int col);
+    Tensor<T> operator*(Tensor &b);
+    Tensor<T> operator*(const T b) const;
+    Tensor<T> operator+(const Tensor &b) const;
+    Tensor<T> operator+(const T b) const;
+    Tensor<T> operator-(const Tensor &b) const;
+    Tensor<T> operator=(const Tensor &b);
 
 };
 
