28121e572e45a51ae4fdbbada4ab31ee6aff31ce
Leonardo Araujo <leonardo.araujo@apical.co.uk>
2017-03-20 17:10:28 +0000

    Add some emtpy return on DeepLearningModel::Loss, issue #45

--- utils/optimization_functions/cppPrj/classifier/deeplearningmodel.cpp
+++ utils/optimization_functions/cppPrj/classifier/deeplearningmodel.cpp
@@ -6,6 +6,8 @@ Tensor<float> DeepLearningModel::Predict(const Tensor<float> &input){
     return A;
 }
 
-tuple<float, Tensor<float>> DeepLearningModel::Loss(const Tensor<float> &X, const Tensor<float> &Y){
-
+tuple<float, Tensor<float>> DeepLearningModel::Loss(const Tensor<float> &X, const Tensor<float> &Y){    
+    float A = 0.1F;
+    Tensor<float> B(vector<int>({1,2}),{0,0});
+    return make_tuple(A,B);
 }
