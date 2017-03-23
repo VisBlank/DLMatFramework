6c3a25aba043c90d1fc5e3bf87c97a880ad6bac7
Burton2000 <r-burton@hotmail.co.uk>
2017-03-23 15:13:03 +0000

    working on sgd

--- utils/optimization_functions/cppPrj/solver/sgd.cpp
+++ utils/optimization_functions/cppPrj/solver/sgd.cpp
@@ -1,17 +1,21 @@
 #include "sgd.h"
 
 template <typename T>
-SGD<T>::SGD()
+SGD<T>::SGD(config)
 {
-
+    m_config = config;
+    m_base_lr = m_config.at("learning_rate");
+    m_base_lr = 0;
 }
 
 template<typename T>
 Tensor<T> SGD<T>::Optimize(const Tensor<T> &params, const Tensor<T> &grad_params, const OptimizerState<T> &state){
     //cout << "SGD optimizer" << endl;
 
-    Tensor<T> A(vector<int>({1,2}),{0,0});    
-    return A;
+    // Gradient descent approach (Simpliest optimizer, no use of states)
+    auto weights = params - (grad_params*m_base_lr);
+
+    return weights;
 }
 
 /*
