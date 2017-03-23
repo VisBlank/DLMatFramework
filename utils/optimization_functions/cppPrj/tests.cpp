e69f923088187e418b9d8a14d7751cc5bdb38795
Burton2000 <r-burton@hotmail.co.uk>
2017-03-23 17:38:11 +0000

    SGD test
    passing

--- utils/optimization_functions/cppPrj/tests.cpp
+++ utils/optimization_functions/cppPrj/tests.cpp
@@ -453,5 +453,22 @@ TEST_CASE( "Tensor tests "){
 
     }
 
+    SECTION( "SGD test" ){
+        OptimizerState<float> opt;
+        map<string, float> conf_test = {{"L2",0.1f},{"Learning_rate",0.1f}};
+        SGD<float> sgd_test(conf_test);
+        Tensor<float> weights_test(vector<int>({2,2}),{0.1709,-0.0224,0.6261,0.4194});
+        Tensor<float> dWeights_test(vector<int>({2,2}),{-0.0443,0.0501,-0.0443,0.0501});
+        auto testWeights = sgd_test.Optimize(weights_test, dWeights_test, opt);
+        Tensor<float> trueWeights(vector<int>({2,2}),{0.1753,-0.0274,0.6305,0.4144});
+
+        cout << "calculated weights: " << trueWeights << endl;
+        cout << "true weights: " << testWeights << endl;
+        REQUIRE(MathHelper<float>::SumVec( testWeights - trueWeights ) < 0.01);
+
+    }
+
+
+
 
 }
