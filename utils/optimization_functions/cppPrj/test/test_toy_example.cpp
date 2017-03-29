06dcbaebe2cd9a685b5a0a35b0637257fd511d96
Leonardo Araujo <leonardo.araujo@apical.co.uk>
2017-03-29 17:43:10 +0100

    Starting with toy example

--- utils/optimization_functions/cppPrj/test/test_toy_example.cpp
+++ utils/optimization_functions/cppPrj/test/test_toy_example.cpp
@@ -17,13 +17,27 @@
 TEST_CASE( "Toy example 2 layer FC deep test"){
 
     SECTION("toy example"){
+        /*
+        // Create the dataset from a HDF5 file
+        Dataset data = Dataset::LoadHDF5(string("../ToyData.h5"));
 
+        // Define model structure
+        LayerContainer layers;
+        layers <= LayerMetaData{"Input",LayerType::TInput,1,2,1,1};// Rows,Cols,channels,batch-size
+        layers <= LayerMetaData{"FC_1",LayerType::TFullyConnected,4};
+        layers <= LayerMetaData{"Relu_1",LayerType::TRelu};
+        layers <= LayerMetaData{"FC_2",LayerType::TFullyConnected,2};
+        layers <= LayerMetaData{"Softmax",LayerType::SoftMax};
 
+        DeepLearningModel net(layers,LossFactory<MultiClassCrossEntropy>::GetLoss());
 
-
-
-
-
+        // Create solver and train
+        Solver solver(net,data,OptimizerType::T_SGD, map<string,float>{{"learning_rate",0.2},{"L2_reg",0}});
+        solver.SetBatchSize(300);
+        solver.SetEpochs(1000);
+        solver.Train();
+        auto lossHistory = solver.GetLossHistory();
+        */
     }
 
 
