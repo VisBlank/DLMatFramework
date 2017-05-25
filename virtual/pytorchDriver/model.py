af37466dda690f215fd18c49c9cef41d23c528be
leonardo <leonardoaraujo.santos@gmail.com>
2017-05-25 20:12:50 +0100

    Working on PyTorch car training

--- virtual/pytorchDriver/model.py
+++ virtual/pytorchDriver/model.py
@@ -1,3 +1,5 @@
+# References
+# https://github.com/vinhkhuc/PyTorch-Mini-Tutorials
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -6,54 +8,62 @@ class CNNDriver(nn.Module):
     def __init__(self):
         super(CNNDriver, self).__init__()
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 24, kernel_size=5, padding=0, stride=2),
-            nn.ReLU())
+        self.layer1 = nn.Sequential()
+        self.layer1.add_module("conv_1", nn.Conv2d(3, 24, kernel_size=5, padding=0, stride=2))
+        self.layer1.add_module("relu_1", nn.ReLU())
 
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(24, 36, kernel_size=5, padding=0, stride=2),
-            nn.ReLU())
+        self.layer2 = nn.Sequential()
+        self.layer2.add_module("conv_2", nn.Conv2d(24, 36, kernel_size=5, padding=0, stride=2))
+        self.layer2.add_module("relu_2", nn.ReLU())
 
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(36, 48, kernel_size=5, padding=0, stride=2),
-            nn.ReLU())
+        self.layer3 = nn.Sequential()
+        self.layer3.add_module("conv_3", nn.Conv2d(36, 48, kernel_size=5, padding=0, stride=2))
+        self.layer3.add_module("relu_3", nn.ReLU())
 
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(48, 64, kernel_size=3, padding=0, stride=1),
-            nn.ReLU())
+        self.layer4 = nn.Sequential()
+        self.layer4.add_module("conv_4", nn.Conv2d(48, 64, kernel_size=3, padding=0, stride=1))
+        self.layer4.add_module("relu_4", nn.ReLU())
 
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=0, stride=1),
-            nn.ReLU())
+        self.layer5 = nn.Sequential()
+        self.layer5.add_module("conv_5", nn.Conv2d(64, 64, kernel_size=3, padding=0, stride=1))
+        self.layer5.add_module("relu_5", nn.ReLU())
 
-        self.fc1 = nn.Linear(1152, 1164)
+        self.fc1 = nn.Sequential()
+        self.fc1.add_module("fc1", nn.Linear(1152, 1164))
+        self.fc1.add_module("relu_6", nn.ReLU())
+        self.fc1.add_module("dropout_1", nn.Dropout())
 
-        self.fc2 = nn.Linear(1164, 100)
+        self.fc2 = nn.Sequential()
+        self.fc2.add_module("fc2", nn.Linear(1164, 100))
+        self.fc2.add_module("relu_7", nn.ReLU())
+        self.fc2.add_module("dropout_2", nn.Dropout())
 
-        self.fc3 = nn.Linear(100, 50)
+        self.fc3 = nn.Sequential()
+        self.fc3.add_module("fc3", nn.Linear(100, 50))
+        self.fc3.add_module("relu_8", nn.ReLU())
+        self.fc3.add_module("dropout_3", nn.Dropout())
 
-        self.fc4 = nn.Linear(50, 10)
+        self.fc4 = nn.Sequential()
+        self.fc4.add_module("fc4", nn.Linear(50, 10))
+        self.fc4.add_module("relu_9", nn.ReLU())
+        self.fc4.add_module("dropout_4", nn.Dropout())
 
         self.out = nn.Linear(10, 1)
 
     def forward(self, x):
-        layer1 = self.layer1(x)
-        layer2 = self.layer2(layer1)
-        layer3 = self.layer2(layer2)
-        layer4 = self.layer2(layer3)
-        layer4_reshape = layer4.view(layer4.size(0), -1)
+        layer1 = self.layer1.forward(x)
+        layer2 = self.layer2.forward(layer1)
+        layer3 = self.layer3.forward(layer2)
+        layer4 = self.layer4.forward(layer3)
+        layer5 = self.layer5.forward(layer4)
 
-        fc1 = F.relu(self.fc1(layer4_reshape))
-        fc1 = F.dropout(fc1, training=self.training)
+        # Reshape layer5 activation
+        layer5_reshape = layer5.view(layer5.size(0), -1)
 
-        fc2 = F.relu(self.fc2(fc1))
-        fc2 = F.dropout(fc2, training=self.training)
-
-        fc3 = F.relu(self.fc3(fc2))
-        fc3 = F.dropout(fc3, training=self.training)
-
-        fc4 = F.relu(self.fc4(fc3))
-        fc4 = F.dropout(fc4, training=self.training)
+        fc1 = self.fc1.forward(layer5_reshape)
+        fc2 = self.fc2.forward(fc1)
+        fc3 = self.fc3.forward(fc2)
+        fc4 = self.fc4.forward(fc3)
 
         out = self.out(fc4)
         return out
--- virtual/pytorchDriver/train.py
+++ virtual/pytorchDriver/train.py
@@ -2,6 +2,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torchvision.transforms as transforms
+from drive_dataset import DriveData
+from torch.utils.data import DataLoader
 from model import CNNDriver
 
 # Hyper Parameters
@@ -10,8 +12,48 @@ batch_size = 50
 learning_rate = 0.001
 
 cnn = CNNDriver()
-print(cnn)
+# Put model on GPU
+cnn = cnn.cuda()
+
+transformations = transforms.Compose([
+    transforms.RandomCrop((66, 200)),transforms.RandomHorizontalFlip(),
+    transforms.ToTensor()])
+
+# Instantiate a dataset
+dset_train = DriveData('./Track1_Wheel/', transformations)
+
+train_loader = DataLoader(dset_train,
+                          batch_size=400,
+                          shuffle=True,
+                          num_workers=1, # 1 for CUDA
+                          pin_memory=True # CUDA only
+                         )
+
 
 # Loss and Optimizer
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+print('Train size:',len(dset_train), 'Batch size:', 400)
+print('Batches per epoch:',len(dset_train) // batch_size)
+# Train the Model
+for epoch in range(num_epochs):
+    iteration_count = 0
+    for batch_idx, (images, labels) in enumerate(train_loader):
+        images = Variable(images)
+        labels = Variable(labels)
+
+        # Send images and labels to GPU
+        images, labels = images.cuda(), labels.cuda()
+
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = cnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx == 0:
+            print('Epoch [%d/%d] Loss: %.4f' % (epoch + 1, num_epochs, loss.data[0]))
+
+        iteration_count += 1
